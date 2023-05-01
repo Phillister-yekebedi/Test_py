e14d6005fd753e1c483d3c6eeba645a7f13ba155
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-01 23:59:36 +0300

    test

---
+++ test.py
@@ -0,0 +1,59 @@
+# Write a Python function that takes two arguments (a and b) and returns their sum.
+def addition ( a, b):
+    sum = a + b
+    return sum
+
+print(addition(100,100))
+
+# Write a Python function that takes a string as input and returns the string reversed  
+def str(word):
+   return [::-1]
+
+print(str('Wonderful'))
+    
+   
+# Write a Python function that takes a list of integers as input and returns the sum of 
+# all the integers in the list.
+def numb(lists):
+     return sum(list)
+
+sum= [1,3,4,5,6,8]
+print(sum)
+
+
+
+# Write a Python function that takes a list of integers as input and returns a new list 
+# with all the even numbers removed.
+def even():
+   numbers = []
+   x= range(0.10)
+   for i in x:
+      if i % 2 == 0:
+         numbers.append(i)
+         return numbers
+
+print(even)
+
+
+# Write a Python function that takes a list of integers as input and returns the 
+# highest value in the list.
+def many(nums):
+    highest_value= 0
+    for num in nums:
+        if num > highest_value:
+            return highest_value
+    
+print(many[45,89,34,89,9])
+   
+
+
+# Write a Python function that takes a list of strings as input and returns a new
+# list with all the strings capitalized.
+def capitalizes(word):
+    capitalize =  word.upper()
+    return capitalize
+
+print(word(hopperlab))
+
+
+
